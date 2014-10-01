adc483b8a231c91be277b0915267c6ed3e934d96
J Armstrong <jssc29@gmail.com>
2014-10-01 16:45:09 -0400

    Add/fix new comment functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,6 +4,13 @@ get '/' do
   erb :index
 end
 
+post '/comment/new/:id' do
+	@article = Article.find_by(id: params[:id])
+	@comment = Comment.create(user_id: session[:current_user], content: params[:content], article: @article)
+
+	redirect "/article/#{@article.id}"
+end
+
 get '/article/:id' do
 	@article = Article.find_by(id: params[:id])
 	@comments = Comment.where(article_id: params[:id])
