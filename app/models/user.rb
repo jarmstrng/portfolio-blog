b5cdd825de7c99e439ec2208175ecbbb687bbb1f
J Armstrong <jssc29@gmail.com>
2014-10-02 07:57:20 -0400

    Add name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 
   has_many :comments
 
+  validates :name, presence: :true
   validates :email, presence: :true, uniqueness: :true
   validates :password_digest, presence: :true
 
